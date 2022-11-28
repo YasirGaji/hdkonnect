7369a876c336c6c3f3456b32d2ec3273ea0a0d48
Yasir Gaji <gajiyasir@gmail.com>
2022-11-28 01:04:25 +0100

    building the navbar learnt about custom styling in tailwind using @layer

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
 import tw from 'twin.macro'
+import Link from 'next/link'
 
 function Header() {
   return (
     <header>
-      <StyledDiv>
-        <Image src="https://i.ibb.co/hVC5zgW/hdkonnect.jpg" objectFit="contain" layout="fill" alt="logo" />
-      </StyledDiv>
+      <OuterDiv>
+        <Link href="/">
+          <StyledDiv>
+            <Image src="https://i.ibb.co/rd0nsrb/Untitled-design.jpg" objectFit="contain" layout="fill" alt="logo" />
+          </StyledDiv>
+        </Link>
+      </OuterDiv>
+
+      <NavBar>
+        <a className='headerLink'>Products</a>
+        <a className='headerLink'>Explore</a>
+        <a className='headerLink'>Support</a>
+        <a className='headerLink'>Business</a>
+      </NavBar>
     </header>
   )
 }
@@ -15,7 +27,29 @@ function Header() {
 export default Header;
 
 const StyledDiv = tw.div`
-  
+  relative
+  h-10
+  w-10
+  cursor-pointer
+  opacity-75
+  transition
+  hover:opacity-100
+`;
+
+const OuterDiv = tw.div`
+  flex
+  items-center
+  justify-center
+  md:w-1/5
+`;
+
+const NavBar = tw.div`
+  hidden
+  flex-1
+  items-center
+  justify-center
+  space-x-8
+  md:flex
 `;
 
 //https://images.unsplash.com/photo-1615655406736-b37c4fabf923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80 
