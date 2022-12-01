074705e297a0557e6d6c5110426e80ead4385502
Yasir Gaji <gajiyasir@gmail.com>
2022-12-01 23:25:22 +0100

    sanity file installed

---
+++ sanity.js
@@ -0,0 +1,16 @@
+import { createClient } from 'sanity-client'
+import createImageUrlBuilder from '@sanity/image-url'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  token: process.env.SANITY_API_TOKEN,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-10-21", // use current UTC date - see "specifying API version"!
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+export const sanityClient = createClient(config);
+
+
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
