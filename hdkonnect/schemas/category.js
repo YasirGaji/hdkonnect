5aacc5e202892274586d7a9d0e2663695fcb3fa4
Yasir Gaji <gajiyasir@gmail.com>
2022-12-02 16:58:11 +0100

    deleted the unwanted schemas

--- hdkonnect/schemas/category.js
+++ hdkonnect/schemas/category.js
@@ -1,13 +1,17 @@
+import { BiCategory } from 'react-icons/bi'
+
 export default {
   name: 'category',
   title: 'Category',
   type: 'document',
+  icon: BiCategory,
   fields: [
     {
       name: 'title',
       title: 'Title',
       type: 'string',
     },
+
     {
       name: 'slug',
       title: 'Slug',
@@ -15,23 +19,59 @@ export default {
       options: {
         source: 'title',
         maxLength: 96,
-      },
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-    {
-      name: 'parents',
-      title: 'Parent categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'category'}],
-        },
-      ],
-    },
-  ],
+      }
+    }
+  ]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default {
+//   name: 'category',
+//   title: 'Category',
+//   type: 'document',
+//   fields: [
+//     {
+//       name: 'title',
+//       title: 'Title',
+//       type: 'string',
+//     },
+//     {
+//       name: 'slug',
+//       title: 'Slug',
+//       type: 'slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 96,
+//       },
+//     },
+//     {
+//       name: 'description',
+//       title: 'Description',
+//       type: 'text',
+//     },
+//     {
+//       name: 'parents',
+//       title: 'Parent categories',
+//       type: 'array',
+//       of: [
+//         {
+//           type: 'reference',
+//           to: [{type: 'category'}],
+//         },
+//       ],
+//     },
+//   ],
+// }
