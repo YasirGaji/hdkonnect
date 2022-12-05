6fc555415e5ca33ed551c5a7e1b9bbb8f74117a5
Yasir Gaji <gajiyasir@gmail.com>
2022-12-05 02:55:34 +0100

    completed the product component

--- Components/Product.tsx
+++ Components/Product.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { urlFor } from '../sanity';
 import tw from 'twin.macro'
+import { ShoppingCartIcon } from '@heroicons/react/outline';
 
 interface Props {
   product: Product;
@@ -9,7 +10,7 @@ interface Props {
 
 function Product({product} : Props) {
   return (
-    <div>
+    <FirstDiv>
       <InnerDiv>
         <Image 
           src={urlFor(product.image[0]).url()} 
@@ -18,15 +19,68 @@ function Product({product} : Props) {
           objectFit="contain"
         />
       </InnerDiv>
-    </div>
+
+      <InnerDiv2>
+        <SubDiv>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </SubDiv>
+
+        <IconDiv className='flex-shrink-0'>
+          <ShoppingCartIcon className='h-8 w-8 text-white' />
+        </IconDiv>
+      </InnerDiv2>
+    </FirstDiv>
   )
 }
 
 export default Product;
+
+const FirstDiv = tw.div`
+  flex 
+  h-fit
+  w-[320px]
+  select-none
+  flex-col
+  space-y-3
+  rounded-xl
+  bg-[#35383c]
+  p-8
+  md:h-[500px]
+  md:w-[400px]
+  md:p-10
+`;
 
 const InnerDiv = tw.div`
   relative 
   w-full 
   h-64
   md:h-72
+`;
+
+const InnerDiv2 = tw.div`
+  flex
+  flex-1
+  justify-between
+  space-x-3
+`;
+
+const SubDiv = tw.div`
+  space-y-2
+  text-xl
+  text-white
+  md:text-2xl
+`;
+
+const IconDiv = tw.div`
+  flex
+  h-16
+  w-16
+  cursor-pointer
+  items-center
+  justify-center
+  rounded-full
+  bg-[#1C1C1C]
+  md:h-[70px]
+  md:w-[70px]
 `;
