3765ed9e2ded693267cf7ca5419509037edacb56
Yasir Gaji <gajiyasir@gmail.com>
2022-12-06 06:54:27 +0100

    refactoring the cart Slice

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    Cart: cartReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
