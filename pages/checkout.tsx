91bb0286cf8cf65ed44cf40371964ce143c50ce5
Yasir Gaji <gajiyasir@gmail.com>
2022-12-06 15:16:49 +0100

    installed paystack

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import Head from 'next/head';
+import React from 'react';
+import Header from '../Components/Header';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Currency from 'react-currency-formatter';
+import  { useSelector }  from  'react-redux' ;
+import { usePaystackPayment } from 'react-paystack';
 
 function Checkout() {
   return (
-    <div>checkout</div>
+    <div>
+      <Head>
+        <title>Cart - HDKonnect</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main>
+        <div>
+          <h1>
+            {}
+          </h1>
+        </div>
+      </main>
+    </div>
   )
 }
 
