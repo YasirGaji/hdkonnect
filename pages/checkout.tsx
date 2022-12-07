0b653f504ac213e2bb5a2316c80d3aeca6fa1e9b
Yasir Gaji <gajiyasir@gmail.com>
2022-12-07 03:50:14 +0100

    bismillah

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,14 +2,24 @@ import Head from 'next/head';
 import React from 'react';
 import Header from '../Components/Header';
 import { ChevronDownIcon } from '@heroicons/react/solid';
-import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Currency from 'react-currency-formatter';
+// import Currency from 'react-currency-formatter';
 import  { useSelector }  from  'react-redux' ;
 import { usePaystackPayment } from 'react-paystack';
+import Button from '../Components/Button';
+// import CheckoutProduct from '../Components/CheckoutProduct';
+import { selectCartItems, selectCartTotal } from '../redux/cartSlice';
+// import { fetchPostJSON } from '../utils/api-helpers';
+// import getPaystack from '../utils/get-paystack';
+import tw from 'twin.macro';
+
+
 
 function Checkout() {
+  const items = useSelector(selectCartItems);
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -21,13 +31,35 @@ function Checkout() {
 
       <main>
         <div>
-          <h1>
-            {}
-          </h1>
+          <H1>
+            {items.length > 0 ? 'Shopping Cart' : 'Your cart is empty'}
+          </H1>
+          <p>We accept returns and Exchanges</p>
+
+          {items.length === 0 &&(
+            <Button 
+              title='Continue Shopping'
+              onClick={() => router.push("/")}
+            />
+          )}
         </div>
+
+        {items.length > 0 && (
+          <div>
+            Yo
+          </div>
+        )}
       </main>
     </div>
   )
 }
 
 export default Checkout;
+
+const H1 = tw.h1`
+  my-4
+  text-3xl
+  font-semibold
+  lg:text-4xl
+
+`;
