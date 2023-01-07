b6d51a0c0cc35458f1ed5a86af1b367344e39b42
Yasir Gaji <gajiyasir@gmail.com>
2023-01-07 10:15:21 +0100

    setup the auth folder and files as well as the session provider in the _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,13 +21,16 @@ import "tailwindcss/tailwind.css";
 import { Provider } from 'react-redux'
 import { store } from '../redux/store';
 import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-    <Provider store={store}>
-      <Toaster />
-      <Component {...pageProps} />
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Toaster />
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
   )
 }
 
